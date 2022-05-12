37f3be9fd8f8d3af2d2a6f7ecf0a90d3361e9dbe
Programming Hero <programming.hero1@gmail.com>
2022-05-11 20:41:01 -0500

    load available time slot

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,8 +7,9 @@ const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
+    const formattedDate = format(date, 'PP');
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
