edacc1a17f8ce431f5d3dced9e647608aec12f2b
Programming Hero <programming.hero1@gmail.com>
2022-05-13 00:51:55 -0500

    make an admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({user}) => {
-    const {email} = user;
+const UserRow = ({user, refetch}) => {
+    const {email, role} = user;
+    const makeAdmin = () =>{
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data =>{
+            refetch();
+            toast.success(`Successfully made an admin`);
+
+        })
+    }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Remove User</button></td>
         </tr>
     );
 };
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,12 @@ import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers:{
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
@@ -26,6 +31,7 @@ const Users = () => {
                            users.map(user=><UserRow
                            key={user._id}
                            user={user}
+                           refetch={refetch}
                            ></UserRow>)
                        }
                     </tbody>
