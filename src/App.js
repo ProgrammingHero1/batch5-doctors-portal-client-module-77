4dfb2553176aafca5145984fc4fe9db1a76538ee
Programming Hero <programming.hero1@gmail.com>
2022-05-13 09:43:44 -0500

    create require admin to protect admin only route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyAppointments from './Pages/Dashboard/MyAppointments';
 import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
+import RequireAdmin from './Pages/Login/RequireAdmin';
 
 function App() {
   return (
@@ -31,7 +32,7 @@ function App() {
           <Route index element={<MyAppointments></MyAppointments>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
-          <Route path="users" element={<Users></Users>}></Route>
+          <Route path="users" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 const useAdmin = user => {
     const [admin, setAdmin] = useState(false);
+    const [adminLoading, setAdminLoading] = useState(true);
     useEffect( () =>{
         const email = user?.email;
         if(email){
@@ -15,10 +16,12 @@ const useAdmin = user => {
             .then(res=>res.json())
             .then(data => {
                 setAdmin(data.admin);
+                setAdminLoading(false);
             })
         }
     }, [user])
-    return [admin]
+
+    return [admin, adminLoading]
 }
 
 export default useAdmin;
