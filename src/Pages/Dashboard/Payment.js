b28d74af0d7349d5bea25bfe070f9c817c142301
Programming Hero <programming.hero1@gmail.com>
2022-05-15 15:58:01 -0500

    simple credit card form setup

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51KzgdMJVa6zVY99CaGts94G8qqJirQWPMAET7VBqrec0wWSxhuuRtgQNPA3SuwzjQKOv6QWwjgMWEfZ83N1qLNUU00IX1ciL6e');
 
 const Payment = () => {
     const { id } = useParams();
@@ -30,7 +35,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
