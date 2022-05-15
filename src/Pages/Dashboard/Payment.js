1d3b858b8bd8596acb41fe3de9a5585be31dd8e7
Programming Hero <programming.hero1@gmail.com>
2022-05-15 07:41:11 -0500

    display appotinemnt specific information on the payment page

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
 const Payment = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
+
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2 className="text-2xl text-purple-500">Please Play for: {id}</h2>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className="text-success font-bold">Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your Appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+                </div>
+            </div>
         </div>
     );
 };
